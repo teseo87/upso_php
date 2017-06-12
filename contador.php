e752567ae49b33dd532a2b814b75390bc6716384
teseo87 <jose.fantasia@geneos.com.ar>
2017-06-12 18:53:09 -0300

    Update contador.php

--- contador.php
+++ contador.php
@@ -1,17 +1,30 @@
 <?php
-session_register('contador');
+
+session_start();
+
 ?>
+
 <HTML>
 <HEAD>
 <TITLE>contador.php</TITLE>
 </HEAD>
 <BODY>
+
 <?php
-if(isset($contador)==0) {
-  $contador=0;
+
+
+
+if(isset($_SESSION["contador"]) == 0) {
+	$_SESSION["contador"] = 0;
 }
-++$contador;
-echo "<a href='contador.php'>Has recargado esta página $contador veces</a>";
+	
+$_SESSION["contador"] = $_SESSION["contador"]++;
+
+	
+	
+echo "<a href='contador.php'>Has recargado esta página " . $contador . " veces</a>";
+
 ?>
+
 </BODY>
 </HTML>
